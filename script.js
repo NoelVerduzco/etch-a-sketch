35879d2b7de501219fb4b2bd1b5a6be9a28f6c76
noelverduzco <noeljverduzco02@gmail.com>
2023-02-01 09:34:45 -0800

    Prompt user for grid length

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // GET USER GRID SIZE
 
+function askGridLength() {
+    let gridLength = prompt("Enter the dimensions of your grid");
+    return gridLength;
+}
 
 const gridBtn = document.querySelector('#gridBtn');
 
 gridBtn.addEventListener('click', function() {
-
+    let gridSide = askGridLength();
+    createGrid(gridSide);
 });
 
 // CREATE GRID
@@ -13,22 +18,23 @@ const pixels = document.querySelector('.pixels');
 
 const fragment = document.createDocumentFragment();
 
-let gridLength = 16;
+function createGrid(num) {
+    let gridLength = num;
 
-for (let row = 0; row < gridLength; row++) {
-    const divRow = document.createElement('div');
-    divRow.classList.add('row');
-    for (let col = 0; col < gridLength; col++) {
-        const divCol = document.createElement('div');
-        divCol.classList.add('col');
-        divRow.appendChild(divCol);
+    for (let row = 0; row < gridLength; row++) {
+        const divRow = document.createElement('div');
+        divRow.classList.add('row');
+        for (let col = 0; col < gridLength; col++) {
+            const divCol = document.createElement('div');
+            divCol.classList.add('col');
+            divRow.appendChild(divCol);
+        }
+        fragment.appendChild(divRow);
     }
-    fragment.appendChild(divRow);
+    pixels.appendChild(fragment);
 }
 
 // COLOR BOXES
-
-pixels.appendChild(fragment);
 
 const cols = document.querySelectorAll('.col');
 
